954f339d854e950dcab3dc605fa4b84e51c933bf
Alex <al-an2017@rambler.ru>
2022-11-02 02:14:08 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает трехзначное число и показыват вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    int num1 = num / 10;
+    int num2 = num1%10;
+    System.Console.WriteLine(num2);
+}
+else{
+    System.Console.WriteLine("Число не трехзначное");
+}
