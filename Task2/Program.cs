28d64792d9d6d4c765de0feaa4af1e4b35851fc3
Alex <al-an2017@rambler.ru>
2022-11-02 07:57:39 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите любое число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num>99)
+{
+    while (num > 1000)
+    {
+      num /=10;  
+    }
+    int result = num % 10;
+    Console.WriteLine(result);
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет"); 
+}
